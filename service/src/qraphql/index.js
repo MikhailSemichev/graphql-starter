3d6beb54bed65e523600290708a740450faf2497
Mikhail Semichev <mikhail_semichev@epam.com>
2018-10-29 01:06:29 +0400

    Decorators experiment

--- service/src/qraphql/index.js
+++ service/src/qraphql/index.js
@@ -6,6 +6,44 @@ const bookSchema = require('./book/bookSchema');
 const userSchema = require('./user/userSchema');
 
 module.exports = {
-    typeDefs: helloSchema.typeDefs + authorSchema.typeDefs + bookSchema.typeDefs + userSchema.typeDefs,
-    resolvers: merge(helloSchema.resolvers, authorSchema.resolvers, bookSchema.resolvers, userSchema.resolvers),
+    typeDefs:
+        helloSchema.typeDefs +
+        authorSchema.typeDefs +
+        bookSchema.typeDefs +
+        userSchema.typeDefs,
+    resolvers: merge(
+        helloSchema.resolvers,
+        authorSchema.resolvers,
+        bookSchema.resolvers,
+        userSchema.resolvers
+    ),
 };
+
+debugger;
+setTimeout(function() {
+    try {
+        const p1 = new Person();
+        p1.sayHi('Ivan');
+    } catch (err) {
+       const ar = err;
+       const errr = ar;
+    }
+}, 200);
+
+//@log
+class Person {
+    @log
+    sayHi(name) {
+        return `Hello ${name}`;
+    }
+}
+
+function log(target, name, descriptor) {
+    const originalFunction = descriptor.value;
+
+    // eslint-disable-next-line
+    descriptor.value = function (...args) {
+        console.log('logger: ', ...args);
+        return originalFunction.apply(this, args);
+    };
+}
--- service/src/server.js
+++ service/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 
 const { ApolloServer } = require('apollo-server-express');
 
