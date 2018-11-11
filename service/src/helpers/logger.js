3ad89768d85d427761bf4134ca7a3fe5246be822
Mikhail Semichev <mikhail_semichev@epam.com>
2018-11-11 12:31:07 +0400

    Fix logger swallow errors problem

--- service/src/helpers/logger.js
+++ service/src/helpers/logger.js
@@ -8,21 +8,22 @@ const logger = createLogger({
         format.colorize(),
         format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
     ),
+    transports: [
+        new transports.File({ filename: 'logs/all.log', timestamp: true }),
+        new transports.File({ filename: 'logs/error.log', level: 'error', timestamp: true }),
+        new transports.Console({ level: 'crit' }), // Slack Channel message
+    ],
+    /*
     exceptionHandlers: [
         new transports.File({
             filename: 'logs/unhandledErrors.log', timestamp: true,
             handleExceptions: true,
         }),
     ],
-    transports: [
-        new transports.File({ filename: 'logs/all.log', timestamp: true }),
-        new transports.File({ filename: 'logs/error.log', level: 'error', timestamp: true }),
-        new transports.Console({ level: 'crit' }), // Slack Channel message
-    ],
     handleExceptions: true,
     exitOnError: true,
+    */
 });
-
 
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new transports.Console());
